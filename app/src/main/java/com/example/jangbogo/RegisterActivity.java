cfd7e844bb4948cd2ba9e28245319dc76430e1ab
Park_HyunSeo <withgustj@naver.com>
2021-10-31 03:15:19 +0900

    RegisterActivity update database

--- app/src/main/java/com/example/jangbogo/RegisterActivity.java
+++ app/src/main/java/com/example/jangbogo/RegisterActivity.java
@@ -1,15 +1,11 @@
 package com.example.jangbogo;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,12 +21,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
-
-    private static final String TAG = "RegisterActivity";
-    EditText mName, mPhoneText, mEmailText, mPasswordText, mSellnumText;
-    TextView mSubmitBtn;
     private FirebaseAuth firebaseAuth;
-
+    private EditText email_Et, password_Et, name_Et, phone_Et, sellnum_Et;
+    private TextView submit_Tv;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,92 +32,74 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.register);
 
 
-        //파이어베이스 접근 설정
-        // user = firebaseAuth.getCurrentUser();
-        firebaseAuth =  FirebaseAuth.getInstance();
-        //firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance(); //접근권한
+        mDatabase = FirebaseDatabase.getInstance().getReference("Users");
 
-        mName = findViewById(R.id.name_Et);
-        mPhoneText = findViewById(R.id.phone_Et);
-        mEmailText = findViewById(R.id.email_Et);
-        mPasswordText = findViewById(R.id.password_Et);
-        mSellnumText = findViewById(R.id.sellnum_Et);
-        mSubmitBtn = findViewById(R.id.submit_btn);
+        email_Et = findViewById(R.id.email_Et);
+        password_Et = findViewById(R.id.password_Et);
+        name_Et = findViewById(R.id.name_Et);
+        phone_Et = findViewById(R.id.phone_Et);
+        sellnum_Et = findViewById(R.id.sellnum_Et);
 
-        //파이어베이스 user 로 접글
-
-        //가입버튼 클릭리스너   -->  firebase에 데이터를 저장한다.
-        mSubmitBtn.setOnClickListener(new View.OnClickListener(){
-
+        submit_Tv = (TextView) findViewById(R.id.submit_Tv);
+        submit_Tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = email_Et.getText().toString().trim();
+                String password = password_Et.getText().toString().trim();
 
-                //가입 정보 가져오기
-                final String email = mEmailText.getText().toString().trim();
-                String pwd = mPasswordText.getText().toString().trim();
-
-
-                if(!pwd.equals("")) {
-                    Log.d(TAG, "등록 버튼 " + email + " , " + pwd);
-                    final ProgressDialog mDialog = new ProgressDialog(RegisterActivity.this);
-                    mDialog.setMessage("가입중입니다...");
-                    mDialog.show();
-
-                    //파이어베이스에 신규계정 등록하기
-                    firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-
-                            //가입 성공시
-                            if (task.isSuccessful()) {
-                                mDialog.dismiss();
-
-                                FirebaseUser user = firebaseAuth.getCurrentUser();
-                                String email = user.getEmail();
-                                String uid = user.getUid();
-                                String name = mName.getText().toString().trim();
-
-                                //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
-                                HashMap<Object,String> hashMap = new HashMap<>();
-
-                                hashMap.put("uid",uid);
-                                hashMap.put("email",email);
-                                hashMap.put("name",name);
-
-                                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                                DatabaseReference reference = database.getReference("Users");
-                                reference.child(uid).setValue(hashMap);
-
-
-                                //가입이 이루어져을시 가입 화면을 빠져나감.
-                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                                Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
-
-                            } else {
-                                mDialog.dismiss();
-                                Toast.makeText(RegisterActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
-                                return;  //해당 메소드 진행을 멈추고 빠져나감.
-
-                            }
-
-                        }
-                    });
-
-                    //비밀번호 오류시
-                }else{
-
-                    Toast.makeText(RegisterActivity.this, "다시 입력해 주세요.", Toast.LENGTH_SHORT).show();
-                    return;
+                if (!email.equals("") && !password.equals("")) {
+                    // 이메일과 비밀번호가 공백이 아닌 경우
+                    createUser(email, password);
+                } else if (email.equals("")) {
+                    // 이메일과 비밀번호가 공백인 경우
+                    Toast.makeText(RegisterActivity.this, "이메일을 입력하세요.", Toast.LENGTH_LONG).show();
+                } else if (password.equals("")) {
+                    // 이메일과 비밀번호가 공백인 경우
+                    Toast.makeText(RegisterActivity.this, "비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "입력 오류", Toast.LENGTH_LONG).show();
                 }
             }
         });
-
     }
 
-    public boolean onSupportNavigateUp(){
-        onBackPressed();; // 뒤로가기 버튼이 눌렸을시
-        return super.onSupportNavigateUp(); // 뒤로가기 버튼
+    private void createUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 회원가입 성공시
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            String uid = user.getUid();
+                            String email = user.getEmail();
+                            String password = password_Et.getText().toString().trim();
+                            String name = name_Et.getText().toString().trim();
+                            String phone = phone_Et.getText().toString().trim();
+                            String sellNum = sellnum_Et.getText().toString().trim();
+
+                            //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                            HashMap<Object,String> hashMap = new HashMap<>();
+
+                            hashMap.put("uid",uid);
+                            hashMap.put("email", email);
+                            hashMap.put("password", password);
+                            hashMap.put("name", name);
+                            hashMap.put("phone",phone);
+                            hashMap.put("sellNum",sellNum);
+
+                            mDatabase.child(uid).setValue(hashMap);
+
+                            Toast.makeText(RegisterActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                            finish();
+                            Intent intent = new Intent(RegisterActivity.this, loginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // 계정이 중복된 경우
+                            Toast.makeText(RegisterActivity.this, "이미 존재하는 계정입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
