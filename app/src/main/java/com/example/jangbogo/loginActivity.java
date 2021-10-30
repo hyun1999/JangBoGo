305dd2927a2e2c707a5576bf4aa0ab1fef36c2ee
Park_HyunSeo <withgustj@naver.com>
2021-10-31 00:50:11 +0900

    login.xml update scrollview loginActivity update RegisterActivity update

--- app/src/main/java/com/example/jangbogo/loginActivity.java
+++ app/src/main/java/com/example/jangbogo/loginActivity.java
@@ -1,14 +1,76 @@
 package com.example.jangbogo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class loginActivity extends AppCompatActivity {
+
+    TextView mLoginBtn;
+    TextView mRegisterTv;
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+
+        firebaseAuth =  FirebaseAuth.getInstance();
+        //버튼 등록하기
+        mRegisterTv = findViewById(R.id.register_Tv);
+        mLoginBtn = findViewById(R.id.login_btn);
+        mEmailText = findViewById(R.id.emailEt);
+        mPasswordText = findViewById(R.id.passwordEt);
+
+
+        //가입 버튼이 눌리면
+        mRegisterTv.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //intent함수를 통해 register액티비티 함수를 호출한다.
+                startActivity(new Intent(loginActivity.this,RegisterActivity.class));
+
+            }
+        });
+
+        //로그인 버튼이 눌리면
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(loginActivity.this,"로그인 버튼",Toast.LENGTH_SHORT).show();
+//                String email = mEmailText.getText().toString().trim();
+//                String pwd = mPasswordText.getText().toString().trim();
+//                firebaseAuth.signInWithEmailAndPassword(email,pwd)
+//                        .addOnCompleteListener(loginActivity.this, new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                if(task.isSuccessful()){
+//                                    Intent intent = new Intent(loginActivity.this, MainActivity.class);
+//                                    startActivity(intent);
+//
+//                                }else{
+//                                    Toast.makeText(loginActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+//                                }
+//                            }
+//                        });
+//
+            }
+        });
     }
 }
+//https://call203.tistory.com/28
