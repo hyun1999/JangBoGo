737b26e96c3d304a4d919b32f4605c333e75b0b4
Park_HyunSeo <withgustj@naver.com>
2021-11-05 23:27:57 +0900

    update main_database

--- app/src/main/java/com/example/jangbogo/MainActivity.java
+++ app/src/main/java/com/example/jangbogo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.jangbogo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,20 +11,73 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     ImageView home_Iv, board_Iv;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<list> arrayList;
+    private FirebaseDatabase database;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        recyclerView = findViewById(R.id.recyclerView); //아이디 연결
+        recyclerView.setHasFixedSize(true); //리사이클러뷰 기존성능 강화
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); //User 객체를 담을 어레이 리스트
+
+        database = FirebaseDatabase.getInstance();//파이어베이스 데이터베이스 연동
+
+
+
+        DatabaseReference databaseReference = database.getReference("Users"); // 데이터베이스 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //파이어베이스 데이터베이스의 데이터를 받아오는곳
+                arrayList.clear(); //초기화
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    list List = snapshot1.getValue(list.class); //만들어뒀던 list 객체에 데이터 담기
+                    arrayList.add(List); //담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼준비
+                }
+                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        adapter = new CustomAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter);
+
+
+
+        //홈버튼 게시판버튼
         home_Iv = (ImageView) findViewById(R.id.home_Iv);
         board_Iv = (ImageView) findViewById(R.id.board_Iv);
         home_Iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               //홈버튼 기능
+                //홈버튼 기능
                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
             }
@@ -29,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
         board_Iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //게시판 버튼 기능
+                //게시판 버튼 기능
                 Intent intent = new Intent(getApplicationContext(),BoardActivity.class);
                 startActivity(intent);
             }
