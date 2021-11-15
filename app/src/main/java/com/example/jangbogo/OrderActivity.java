c148b18363404ea5b5d4853986684950065e420d
Hyunseo Park <withgustj@naver.com>
2021-11-15 20:43:41 +0900

    update search

--- app/src/main/java/com/example/jangbogo/OrderActivity.java
+++ app/src/main/java/com/example/jangbogo/OrderActivity.java
@@ -2,13 +2,27 @@ package com.example.jangbogo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class OrderActivity extends AppCompatActivity {
+    ImageView home_Order_Iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order);
+
+        home_Order_Iv = (ImageView) findViewById(R.id.home_Order_Iv);
+        home_Order_Iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //홈버튼 기능
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
